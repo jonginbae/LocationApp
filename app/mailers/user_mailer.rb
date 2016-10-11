00bc5e57c1b409f05dc31c46ae808b2b7e0234b9
jonginbae <au.jib0610@gmail.com>
2016-10-11 12:30:47 +1100

    fix mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
 
 default from: "beset0610@naver.com"
 
-def welcome(user)
+def confirm(user)
   @student = student #@user will be whatever user we pass in to the 'welcome' method
   mail( :to => @student.parent1_email, :subject => "Please confirm #{@student.first_name}'s activity'", :cc => @student.parent2_email )
 end
